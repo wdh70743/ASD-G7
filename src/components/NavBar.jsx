fa99791caccfddc9aeb1dc3c89191d7f18b377f2
Alexander Nosatti <alexnosatti@alexandersmbp2.home>
2024-09-01 18:46:38 +1000

    Added hero section of dashboard. Did hero.jsx and hero.css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <>Dashboard</>
+    <>navBAR GOES HERE</>
 
   )
 }
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import Hero from '../components/Hero';
 
 
 const DashboardPage = () => {
   return (
-    <NavBar />
+    <>
+        <NavBar />
+        <Hero />
+    </>
+
+    
+
   );
 };
 
