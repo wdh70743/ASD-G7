1ba1a7c9e7e7ae383fc9715234a879a6e4e8d35f
Alexander Nosatti <alexnosatti@alexandersmbp2.home>
2024-09-08 22:07:28 +1000

    Dashboard complete + plus other functions complete. Logout needs to be done

---
+++ src/components/Dashboard/Components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Styles/Project.css'; 
+import '../Styles/TaskList.css'; 
+
+const Projects = ({color, title, subtitle}) => {
+  return (
+    <div className="rectangle-box" style={{ backgroundColor: color }}>
+        <h1 className="title">{title}</h1>
+        <p className="subtitle">{subtitle}</p>
+    </div>
+  );
+}
+
+export default Projects
